5fc51c1acd4f011d2aa5f3f5f61babc352893c25
1lgrand <demarzoad@libero.it>
2025-12-08 12:36:57 +0100

    Cartella Nuova

---
+++ Trees/N_Tree/NT_FCNS.h
@@ -0,0 +1,20 @@
+/**
+ * Implementazione dell'albero N-ario con rappresentazione FCNS (First Child Next Sibling).
+ * 
+ * Ogni nodo contiene:
+ * - firstChild: puntatore al primo figlio
+ * - nextSibling: puntatore al fratello successivo
+ * - parent: puntatore al genitore (opzionale)
+ * 
+ * Questa rappresentazione trasforma un albero n-ario in una struttura binaria,
+ * permettendo un numero illimitato di figli per nodo.
+ */
+
+#ifndef NT_FCNS_H
+#define NT_FCNS_H
+
+
+
+
+
+#endif // NT_FCNS_H
